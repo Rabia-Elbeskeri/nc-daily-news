b7550364dbbb1e73571aeed93a7fc4643e60a9a0
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-03-04 23:23:47 +0000

    Update seed.js
    updated seeds.js

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const db = require("../connection")
-const format = require("pg-format");
-
-const { convertTimestampToDate } = require("./utils");
-const { articleData, commentData, topicData, userData } = require('./index');
-
-const seed = ({ topicData, userData, articleData, commentData }) => {
-
-=======
 const db = require("../connection");
 const format = require("pg-format");
 
@@ -15,59 +5,32 @@ const { convertTimestampToDate } = require("./utils");
 const { articleData, commentData, topicData, userData } = require('../data/test-data/index');
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
->>>>>>> 0c2bd3b (updated work)
     return db
         .query("DROP TABLE IF EXISTS comments;")
         .then(() => db.query("DROP TABLE IF EXISTS articles;"))
         .then(() => db.query("DROP TABLE IF EXISTS users;"))
         .then(() => db.query("DROP TABLE IF EXISTS topics;"))
         .then(() => {
-<<<<<<< HEAD
-            return db.query(`
-                CREATE TABLE topics
-                (
-                    slug        VARCHAR(50) PRIMARY KEY,
-                    description TEXT NOT NULL,
-                    img_url     TEXT NOT NULL
-                );`);
-=======
             return db.query(`CREATE TABLE topics
                 (
                     slug        VARCHAR(50) PRIMARY KEY,
                     description VARCHAR NOT NULL,
                     img_url VARCHAR(1000) NOT NULL
                              );`);
->>>>>>> 0c2bd3b (updated work)
         })
         .then(() => {
             return db.query(`
                 CREATE TABLE users
                 (
-<<<<<<< HEAD
-                    username  VARCHAR(50) PRIMARY KEY,
-                    name   TEXT NOT NULL,
-                    avatar_url TEXT NOT NULL
-=======
                     username   VARCHAR(50) PRIMARY KEY,
                     name VARCHAR NOT NULL,
                     avatar_url VARCHAR(1000) NOT NULL
->>>>>>> 0c2bd3b (updated work)
                 );`);
         })
         .then(() => {
             return db.query(`
                 CREATE TABLE articles
                 (
-<<<<<<< HEAD
-                    article_id SERIAL PRIMARY KEY,
-                    title  TEXT NOT NULL,
-                    topic   VARCHAR(50) REFERENCES topics (slug) ON DELETE CASCADE,
-                    author   VARCHAR(50) REFERENCES users (username) ON DELETE CASCADE,
-                    body  TEXT NOT NULL,
-                    created_at  TIMESTAMP DEFAULT NOW(),
-                    votes  INT DEFAULT 0,
-                    article_img_url TEXT NOT NULL
-=======
                     article_id      SERIAL PRIMARY KEY,
                     title VARCHAR NOT NULL,
                     topic    VARCHAR(50) REFERENCES topics (slug) ON DELETE CASCADE,
@@ -77,7 +40,6 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
                     votes           INT       DEFAULT 0,        
                     article_img_url VARCHAR(1000) NOT NULL
 
->>>>>>> 0c2bd3b (updated work)
                 );`);
         })
 
@@ -87,15 +49,9 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
                 (
                     comment_id SERIAL PRIMARY KEY,
                     article_id INT REFERENCES articles (article_id) ON DELETE CASCADE,
-<<<<<<< HEAD
-                    body  TEXT NOT NULL,
-                    votes  INT   DEFAULT 0,
-                    author  VARCHAR(50) REFERENCES users (username) ON DELETE CASCADE,
-=======
                     body       TEXT NOT NULL,
                     votes      INT       DEFAULT 0,
                     author     VARCHAR(50) REFERENCES users (username) ON DELETE CASCADE,
->>>>>>> 0c2bd3b (updated work)
                     created_at TIMESTAMP DEFAULT NOW()
                 );`);
         })
@@ -119,11 +75,6 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
         })
         .then(() => {
             const formattedCommentData = commentData.map(convertTimestampToDate);
-<<<<<<< HEAD
-            const formattedCommentValues = formattedCommentData.map(({ article_id, body, votes, author, created_at }) => {
-                return [article_id, body, votes, author, created_at];
-            });
-=======
 
             const formattedCommentValues = formattedCommentData.map(({ article_id, body, votes, author, created_at }) => {
                 return [article_id, body, votes, author, created_at];
@@ -132,7 +83,6 @@ const seed = ({ topicData, userData, articleData, commentData }) => {
 
 
 
->>>>>>> 0c2bd3b (updated work)
             const commentQuery = format(`INSERT INTO comments (article_id, body, votes, author, created_at) VALUES %L RETURNING *;`, formattedCommentValues);
             return db.query(commentQuery);
         });
@@ -146,30 +96,16 @@ const getAllUsers = () => {
         .catch(err => console.error(err));
 };
 
-<<<<<<< HEAD
-
-const getArticlesByTopic = (topic) => {
-    const query = `SELECT * FROM articles WHERE topic = '${topic}';`;
-    return db.query(query)
-        .then(result => result.rows)
-        .catch(err => console.error(err));
-  
-=======
 const getArticlesByTopic = (topic) => {
     return db.query("SELECT * FROM articles WHERE topic = $1;", [topic])
         .then(result => result.rows)
         .catch(err => console.error(err));
->>>>>>> 0c2bd3b (updated work)
 };
 
 const getCommentsWithNegativeVotes = () => {
     return db.query("SELECT * FROM comments WHERE votes < 0;")
         .then(result => result.rows)
-<<<<<<< HEAD
-        .catch(err => console.error(err));    
-=======
         .catch(err => console.error(err));
->>>>>>> 0c2bd3b (updated work)
 };
 
 const getAllTopics = () => {
@@ -179,35 +115,17 @@ const getAllTopics = () => {
 };
 
 const getArticlesByUser = (username) => {
-<<<<<<< HEAD
-   const query = `SELECT * FROM articles WHERE author = '${username}';`;
-         return db.query(query)
-=======
     return db.query("SELECT * FROM articles WHERE author = $1;", [username])
 
->>>>>>> 0c2bd3b (updated work)
         .then(result => result.rows)
         .catch(err => console.error(err));
 };
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 0c2bd3b (updated work)
 const getPopularComments = () => {
     return db.query("SELECT * FROM comments WHERE votes > 10;")
         .then(result => result.rows)
         .catch(err => console.error(err));
 };
 
-<<<<<<< HEAD
-
-
-
-
-
-
 module.exports = seed;
-=======
-module.exports = seed;
->>>>>>> 0c2bd3b (updated work)
