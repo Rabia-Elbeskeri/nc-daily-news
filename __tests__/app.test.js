0c079a7de86b7721d72ea0507dcde9eb136db6cd
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-03-04 23:12:54 +0000

    Fixed tests and updated dependencies

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,20 @@
 const endpointsJson = require("../endpoints.json");
+const app = require("../app");
+const request = require("supertest");
 /* Set up your test imports here */
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
 
 /* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+    return seed(data);
+});
+
+afterAll(() => {
+    return db.end();
+});
+
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
