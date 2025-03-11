61c4b55618ea3938cf30f9fa80365a8ed5d8f8b3
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-03-11 18:59:54 +0000

    Update topics.js

--- db/data/test-data/topics.js
+++ db/data/test-data/topics.js
@@ -1,20 +1,3 @@
-// module.exports = [
-//   {
-//     description: 'The man, the Mitch, the legend',
-//     slug: 'mitch',
-//     img_url: ""
-//   },
-//   {
-//     description: 'Not dogs',
-//     slug: 'cats',
-//     img_url: ""
-//   },
-//   {
-//     description: 'what books are made of',
-//     slug: 'paper',
-//     img_url: ""
-//   }
-// ];
 
 module.exports = [
   {
