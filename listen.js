2c4613ac0a31e86f3c89755fa49bc92329216257
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-03-27 21:54:24 +0000

    Ready for deployment to Render

--- listen.js
+++ listen.js
@@ -1,10 +1,23 @@
-const app = require("./app");
 
-app.listen(8081, (err) => {
+const app = require("./app");
+const { PORT = 8081 } = process.env;
+
+app.listen(PORT, (err) => {
     if (err) {
         console.log("Error starting server:", err);
     } else {
-        console.log("Server is listening on 8081");
+        console.log(`Server is listening on ${PORT}`);
     }
 });
 
+
+
+// const app = require("./app");
+//
+// app.listen(8081, (err) => {
+//     if (err) {
+//         console.log("Error starting server:", err);
+//     } else {
+//         console.log("Server is listening on 8081");
+//     }
+// });
