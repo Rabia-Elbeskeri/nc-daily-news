6c3ea13f2dd325aeab774350ce588918439b77c9
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-04-01 12:52:40 +0100

    updated

--- controllers/topicsPost.controller.js
+++ controllers/topicsPost.controller.js
@@ -1,11 +1,15 @@
 const { insertTopic } = require("../models/topics.model");
 
 const postTopic = (request, response, next) => {
-    const { slug, description } = request.body;
+    const { slug, description, img_url } = request.body;
 
-    insertTopic(slug, description)
+    insertTopic(slug, description, img_url)
         .then((topic) => response.status(201).send({ topic }))
         .catch(next);
 };
 
 module.exports = { postTopic };
+
+
+
+
